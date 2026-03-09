daacf9ad1197fe22aa06dac3881bb254d448b548
nkechielemchiibe-cmd <nkechielemchiibe@gmail.com>
2026-03-09 01:39:42 -0400

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+# 📌 Table of Contents
+1.  Project Overview  
+2.  Repository Structure  
+3.  12‑Month Awareness Plan  
+4.  Phishing Simulation Templates  
+5.  Training Posters  
+6.  Power BI Dashboard  
+7.  Dataset  
+8.  Incident Response Playbooks  
+9.  Author
